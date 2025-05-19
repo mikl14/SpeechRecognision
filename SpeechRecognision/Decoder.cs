b2784b2509bf12cc21a838969eb910f7af14a570
mikl14 <mikl.eli2002@gmail.com>
2025-05-19 22:12:46 +0300

    Добавьте файлы проекта.

---
+++ SpeechRecognision/Decoder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechRecognision
+{
+    internal class Decoder
+    {
+        public static Dictionary<String, String> wordToCode = new Dictionary<string, string> { };
+
+        public static string decode(String input)
+        {
+            foreach (String key in wordToCode.Keys)
+            {
+                if (input.Contains(key))
+                {
+                    input = input.Replace(key, wordToCode[key]);
+                }
+            }
+            return input;
+        }
+    }
+}
